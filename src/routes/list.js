07fc923815b58571a13dc9912bf5c52e4e577575
Ivan Pashchenko <ivanko.paschenko@gmail.com>
2020-01-22 00:02:50 +0200

    test

--- src/routes/list.js
+++ src/routes/list.js
@@ -3,7 +3,7 @@ module.exports = function(router, db) {
         try {
             const newTask = {...data, user, done: false, createdAt: new Date().getTime()};
             db.collection('task').insertOne(newTask);
-            res.send({success: 'Task has been created'});
+            res.send({success: 'Task has been created!'});
         } catch (e) {
             res.status(500).send(e);
         }
